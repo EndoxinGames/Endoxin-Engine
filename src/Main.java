f2a1723f84389518ab0bd64d2ea16e7c37049d6e
adamsong <adamson.g@gmail.com>
2014-04-04 11:55:32 -0400

    Changed things

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+
 import com.MinotaurGames.engine.Window;
 import com.MinotaurGames.localization.Localization;
 
@@ -8,10 +10,21 @@ public class Main {
 	public static final int HEIGHT = 600;
 	public static final String VERSION = "InDev";
 	public static final String TITLE = "Labyrinthine v" + VERSION;
-	public static final String LANG = "English";
+	/*
+	 * Languages:
+	 *	English
+	 *	Pirate
+	 *	日本語
+	 *	
+	 */
+	public static final String LANG = "日本語";
 	
 	public static void main(String[] args) {
-		Localization.init(LANG);
+		try {
+			Localization.init(LANG);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		Window.createWindow(WIDTH, HEIGHT, TITLE);
 	}
 	
--- src/com/MinotaurGames/localization/Localization.java
+++ src/com/MinotaurGames/localization/Localization.java
@@ -7,36 +7,35 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import com.MinotaurGames.exceptions.LocalizationNotPresentException;
+import com.MinotaurGames.exceptions.LocalizionNotInitializedException;
+
 public class Localization {
 	public static String language;
 	public static HashMap<String, String> languages = new HashMap<String, String>();
 	public static HashMap<String, String[]> localization = new HashMap<String, String[]>();
-	
-	public static void init(String language){
-		try{
+
+	public static void init(String language) throws FileNotFoundException {
+		try {
 			languages = populateRoster();
-		} catch(FileNotFoundException e) {
-			
+		} catch (FileNotFoundException e) {
+
 		}
 		System.out.println("Language set to " + language);
 		Localization.language = language;
-		if(languages.containsKey(language)){
-			File file = new File(new File("").getAbsolutePath().concat("/assets/localizations/" + languages.get(language) + ".lang"));
-			if(!file.canRead()){
+		if (languages.containsKey(language)) {
+			File file = new File("assets/localizations/" + languages.get(language) + ".lang");
+			if (!file.canRead()) {
 				System.out.println("File not found");
 				return;
 			}
 			BufferedReader br = null;
-			try {
-				br = new BufferedReader(new FileReader(file));
-			} catch (FileNotFoundException e) {
-				//TODO Add exception
-			}
+			br = new BufferedReader(new FileReader(file));
 			String line;
 			try {
-				while((line=br.readLine()) != null){
+				while ((line = br.readLine()) != null) {
 					String[] data = line.split(" ");
-					String[] data2 = {data[1], data[2], data[3]};
+					String[] data2 = { data[1], data[2], data[3] };
 					localization.put(data[0], data2);
 				}
 			} catch (IOException e) {
@@ -44,24 +43,21 @@ public class Localization {
 			}
 		}
 	}
-	
-	private static HashMap<String, String> populateRoster() throws FileNotFoundException {
-		File file = new File(new File("").getAbsolutePath().concat("/assets/localizations/languages.txt"));
+
+	private static HashMap<String, String> populateRoster()
+			throws FileNotFoundException {
+		File file = new File("assets/localizations/languages.txt");
 		HashMap<String, String> roster = new HashMap<String, String>();
-		if(!file.canRead()){
+		if (!file.canRead()) {
 			System.out.println("File not found");
 			throw new FileNotFoundException();
 		} else {
 			BufferedReader br = null;
-			try {
-				br = new BufferedReader(new FileReader(file));
-			} catch (FileNotFoundException e) {
-				//TODO Add exception
-			}
+			br = new BufferedReader(new FileReader(file));
 			String line;
 			Integer i = 1;
 			try {
-				while((line=br.readLine()) != null){
+				while ((line = br.readLine()) != null) {
 					String[] data = line.split(":");
 					roster.put(data[0], data[1]);
 					i++;
@@ -73,11 +69,13 @@ public class Localization {
 		}
 		return roster;
 	}
-	
-	public static String localize(String stringToLocalize, int capitalization) throws LocalizionNotInitializedException, LocalizationNotPresentException{
-		if(language != null){
-				String localizedString = localization.get(stringToLocalize)[capitalization];
-				return localizedString;
+
+	public static String localize(String stringToLocalize, int capitalization)
+			throws LocalizionNotInitializedException,
+			LocalizationNotPresentException {
+		if (language != null) {
+			String localizedString = localization.get(stringToLocalize)[capitalization];
+			return localizedString;
 		}
 		throw new LocalizionNotInitializedException();
 	}
